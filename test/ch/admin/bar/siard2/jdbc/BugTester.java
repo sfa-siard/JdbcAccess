86f51a4e30b7c693a906fbdae253d033c792dd4f
Hartwig Thomas <hartwig.thomas@enterag.ch>
2019-12-02 10:22:48 +0100

    Sync with master

---
+++ test/ch/admin/bar/siard2/jdbc/BugTester.java
@@ -0,0 +1,16 @@
+package ch.admin.bar.siard2.jdbc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BugTester
+{
+
+  @Test
+  public void test()
+  {
+    fail("Not yet implemented");
+  }
+
+}
